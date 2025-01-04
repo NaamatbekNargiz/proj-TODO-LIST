5b4947ba1249d93b640b5c2556a87c6534ae9004
Nargiza Naamatbekova <naamatbeknargiza@gmail.com>
2025-01-04 19:27:22 +0600

    todo

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "../Todo/Todo.css";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  };
+
+  const toggleTodo = (id, completed) => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  };
+  const deleteTodo = (id) => {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  return (
+    <>
+      <TodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
